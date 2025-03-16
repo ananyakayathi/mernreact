9b8e1a667bc8f879296d8c79357a3df65441499e
Ananya <ananyakayathi@gmail.com>
2025-03-16 11:51:32 +0530

    testing gh files

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import App12 from './App12';
+import App16 from './App16';
+import App17 from './App17';
+
+const sqrfunction=(n)=>{
+    return n*n;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App12/>)
+root.render(<>
+<App16 val={5} fn={sqrfunction}/>//sending callback as a property
+<App17 val={7} fn={sqrfunction}/>
+</>);
 
+// root.render(<App16 val={5} fn={sqrfunction}/>)
+
+// root.render(<App15 result={{'john':40,'cathy':50}}/>)
+// root.render(<App14 marks={[34,21,56]}/>)
 // root.render(<Main1 flag={true}/>);
-// root.render(<App5 name="Catchy"/>);
+// root.render(<App5 name="Cathy"/>);
 // root.render(<App4 n={5}/>);
 // root.render(<App3/>);
 // root.render(<App flag={true}/>);
